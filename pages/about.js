f791a2fab37493f3460293b7db95666ae19ae5f2
Jared Middleton <jaredmiddleton3.14@gmail.com>
2019-07-18 15:53:02 -0700

    Use Link api to connect index to about page

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+export default function About(){
+    return (
+        <div>
+            <p>This is the about page.</p>
+            <p>I'm just fiddling with the Next.js fw to get a better understanding of nodejs and react :)</p>
+        </div>
+    );
+}
