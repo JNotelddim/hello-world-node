614ae14597e42a01cfcb1d446822a1119de55f05
Jared Middleton <jaredmiddleton3.14@gmail.com>
2019-07-18 15:21:54 -0700

    Update app to use express fw
    am following this tutorial now:
    https://medium.com/@onejohi/building-a-simple-rest-api-with-nodejs-and-express-da6273ed7ca9

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-const http = require('http');
+var express = require("express");
+var app = express();
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/plain');
-      res.end('Hello World\n');
+app.get("/url", (req, res, next) => {
+    res.json(["Tony", "Lisa","Michael","Babra","Kaydance"])
 });
 
-server.listen(port, hostname, () => {
-      console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(3000, ()=>{
+    console.log("Server running on port 3000")
 });
