98e7650b29288398d20939eefbf2f76d175254f2
Jared Middleton <jaredmiddleton3.14@gmail.com>
2019-08-21 14:20:58 -0700

    (4) Create Dynamic Pages
    update index to be Blog homepage, and create dynamically loading `Page` page which iakes the router
    query string and uses it to generate the page title

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,25 @@
 //This is the Link API
 //import Link from 'next/link';
 import Layout from '../comps/MyLayout.js';
+import Link from 'next/link';
 
-export default function Index(){
+const PostLink = props => (
+    <li>
+        <Link href={`/post?title=${props.title}`}>
+            <a>{props.title}</a>
+        </Link>
+    </li>
+)
+
+export default function Blog(){
     return (
         <Layout>
-            <p>Hello Next.js</p>
+            <h1>MyBlog</h1>
+            <ul>
+                <PostLink title="Hello Next.js"></PostLink>
+                <PostLink title="Learn Next.js is awesome"></PostLink>
+                <PostLink title="Deploy apps with Zeit"></PostLink>
+            </ul>
         </Layout>
     );
 };
