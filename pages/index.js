184d236682c4dca47011cd8761ed31491347239d
Jared Middleton <jaredmiddleton3.14@gmail.com>
2019-08-21 15:22:57 -0700

    (7.1) Add styling for reverted contents of homepage
    tutorial scrapped the work for fetching data on the blog homepage, but
    added styling -- I think it will have me readd the my dynamic contents
    soon?

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,52 @@
 //This is the Link API
-//import Link from 'next/link';
 import Layout from '../comps/MyLayout.js';
 import Link from 'next/link';
-import fet from 'isomorphic-unfetch'
 
-const Index = props => (
-    <Layout>
-        <h1></h1>
-        <ul>
-            {props.shows.map( show => (
-            <li key={show.id}>
-                <Link href="/p/[id]" as={`/p/${show.id}`}>
-                    <a>{show.name}</a>
-                </Link>
-            </li>
-            ))}
-        </ul>
-    </Layout>
-);
+function getPosts() {
+    return [
+        { id: "hello-nextjs", title: "Hello Next.js" },
+        { id: "learn-nextjs", title: "Learn Next.js is awesome" },
+        { id: "deploy-nextjs", title: "Deploy apps with Zeit" }
+    ]
+}
 
-Index.getInitialProps = async function() {
-    const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
-    const data = await res.json();
+export default function Blog(){
+    return (
+        <Layout>
+            <h1>My Blog</h1>
+            <ul>
+                {getPosts().map( post => (
+                    <li key={post.id}>
+                        <Link href="/p/[id]" as={`/p/${post.id}`}>
+                            <a>{post.title}</a>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+            <style jsx>{`
+                h1, a {
+                font-family: 'Arial'
+                }
 
-    console.log(`Show data fetched. Count: ${data.length}`);
+                ul {
+                    padding: 0;
+                }
 
-    return {
-        shows: data.map(entry => entry.show)
-    };
-};
+                li {
+                    list-style: none;
+                    margin: none;
+                }
 
-export default Index;
+                a {
+                    text-decoration: none;
+                    color: blue;
+                }
+
+                a:hover {
+                    opacity: 0.6;
+                }
+                `}
+            </style>
+        </Layout>
+    );
+}
