b210ddefa0466903ecb16b6abc9a3a0b39cf8429
Jared Middleton <jaredmiddleton3.14@gmail.com>
2019-08-21 14:48:26 -0700

    (6.1) Fetching Data For Pages
    installed isomorphic unfetch  for fetching data (?)
    then used it in Index.getInitialProps (a static async method which can
    be added to any component to load its data) to fetch shows to list from
    the tvmaze api

--- pages/index.js
+++ pages/index.js
@@ -2,24 +2,32 @@
 //import Link from 'next/link';
 import Layout from '../comps/MyLayout.js';
 import Link from 'next/link';
+import fet from 'isomorphic-unfetch'
 
-const PostLink = props => (
-    <li>
-        <Link href="/p/[id]" as={`/p/${props.id}`}>
-            <a>{props.id}</a>
-        </Link>
-    </li>
-)
+const Index = props => (
+    <Layout>
+        <h1></h1>
+        <ul>
+            {props.shows.map( show => (
+            <li key={show.id}>
+                <Link href="/p/[id]" as={`/p/${show.id}`}>
+                    <a>{show.name}</a>
+                </Link>
+            </li>
+            ))}
+        </ul>
+    </Layout>
+);
 
-export default function Blog(){
-    return (
-        <Layout>
-            <h1>MyBlog</h1>
-            <ul>
-                <PostLink id="Hello Next.js"></PostLink>
-                <PostLink id="Learn Next.js is awesome"></PostLink>
-                <PostLink id="Deploy apps with Zeit"></PostLink>
-            </ul>
-        </Layout>
-    );
+Index.getInitialProps = async function() {
+    const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
+    const data = await res.json();
+
+    console.log(`Show data fetched. Count: ${data.length}`);
+
+    return {
+        shows: data.map(entry => entry.show)
+    };
 };
+
+export default Index;
