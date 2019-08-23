0d360b81e2429f8fa8abdf6f21a59af71c6696c7
Jared Middleton <jaredmiddleton3.14@gmail.com>
2019-08-23 15:03:09 -0700

    (E2.2) Failing NextPage in Typescript Next app
    *the tsconfig.json never came into existance when it was supposed to, so
    I had to make my own??
    *NextPage seems to be expecting type `string` and the jsx rendered by
    Home() seems to be of type `string | string[]` which doesn't make much
    sense to me but I can't seem to get it working -- so I'm going to scrap
    all this and try `create-next-app --example with-typescript` I suppose?

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,16 @@
-let Home = () => <h1>hello world</h1>;
+import { NextPage } from 'next'
+
+interface Props {
+    userAgent?: string
+}
+
+const Home: NextPage<Props> = ({userAgent}) => (
+    <h1>hello world! user agent: {userAgent}</h1>
+);
+
+Home.getInitialProps = async ({req}) => {
+    const userAgent = req ? req.headers['user.agent'] || '' : navigator.userAgent
+    return { userAgent }
+}
 
 export default Home
